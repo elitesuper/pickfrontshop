0ea3b746e036c2a489cda7ca36ffce03f3c59204
Godric Gryffindor <112949204+elitesuper@users.noreply.github.com>
2023-01-17 15:58:15 -0800

    auto_complete test!

--- src/components/otp/code-verify-form.tsx
+++ src/components/otp/code-verify-form.tsx
@@ -1,4 +1,4 @@
-import MobileOtpInput from 'react-otp-input';
+import MobileOtpInput from '@zyda/react-otp-input';
 import Button from '@/components/ui/button';
 import Label from '@/components/ui/forms/label';
 import { useModalAction } from '@/components/ui/modal/modal.context';
@@ -42,6 +42,7 @@ export default function OtpCodeForm({
                 render={({ field: { onChange, onBlur, value } }) => (
                   <MobileOtpInput
                     isInputNum={true}
+                    isInputSecure={true}
                     value={value}
                     onChange={onChange}
                     numInputs={6}
